aa55ce78265fef09b0018cf3d6a4bd50e51a932c
Thalysson Delano <contato.thalyssondel@gmail.com>
2026-02-21 20:38:22 -0300

    fix: Fixing logic in file separation

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -1,6 +1,5 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.core.exceptions import PermissionDenied
-from django.contrib.auth.backends import ModelBackend
 
 class BlockAdminAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
@@ -9,12 +8,4 @@ class BlockAdminAdapter(DefaultSocialAccountAdapter):
         if user and user.pk:
             if user.is_superuser or user.is_staff:
                 raise PermissionDenied("This account could not be authenticated at this time")
-            
-
-class BanCheckBackend(ModelBackend):
-    def authenticate(self, request, username = None, password = None, **kwargs):
-        user = super().authenticate(request, username, password, **kwargs)
-
-        if user and getattr(user, 'is_banned', False):
-            raise PermissionDenied('Your account is banned')
-        return user
+        
